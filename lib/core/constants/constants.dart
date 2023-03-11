20359a0da3df89955dd70ff5b937e0451f95b792
ryan-dxb <ryan.dxb92@gmail.com>
2023-03-11 10:31:28 +0400

    Created Login Screen and added Firebase Google Sign In

---
+++ lib/core/constants/constants.dart
@@ -0,0 +1,5 @@
+class Constants {
+  static const logoPath = 'assets/images/logo.png';
+  static const loginEmoticonPath = 'assets/images/loginEmote.png';
+  static const googleIconPath = 'assets/images/google.png';
+}
---
+++ lib/features/auth/repository/auth_repository.dart
@@ -0,0 +1,45 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:reddit_clone/core/providers/firebase_providers.dart';
+
+final authRepositoryProvider = Provider((ref) => AuthRepository(
+      firestore: ref.read(firestoreProvider),
+      firebaseAuth: ref.read(firebaseAuthProvider),
+      googleSignIn: ref.read(googleSignInProvider),
+    ));
+
+class AuthRepository {
+  final FirebaseFirestore _firestore;
+  final FirebaseAuth _firebaseAuth;
+  final GoogleSignIn _googleSignIn;
+
+  AuthRepository({
+    required FirebaseFirestore firestore,
+    required FirebaseAuth firebaseAuth,
+    required GoogleSignIn googleSignIn,
+  })  : _firestore = firestore,
+        _firebaseAuth = firebaseAuth,
+        _googleSignIn = googleSignIn;
+
+  void singInWithGoogle() async {
+    try {
+      final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+
+      final googleAuth = await googleUser?.authentication;
+
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth?.accessToken,
+        idToken: googleAuth?.idToken,
+      );
+
+      UserCredential userCredential =
+          await _firebaseAuth.signInWithCredential(credential);
+
+      print(userCredential.user?.email);
+    } catch (e) {
+      print(e);
+    }
+  }
+}
